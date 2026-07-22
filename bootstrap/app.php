5117f703dcf02c7d05a9f94ff041c2eff0958e84
Javier Minto Arze <diegominto2@gmail.com>
2026-07-22 12:21:05 -0400

    Ajuste boots

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,21 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    // bootstrap/app.php
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*');
-        $middleware->trustProxies(
-            headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
-            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
-            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
-            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
-            \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
-        );
+        $middleware->alias([
+            'vigente' => \App\Http\Middleware\CheckVigencia::class,
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (AuthenticationException $e, Request $request) {
-            if ($request->expectsJson() || $request->is('whatsapp/*')) {
-                return response()->json(['message' => 'Unauthenticated.'], 401);
-            }
-        });
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
     })->create();
