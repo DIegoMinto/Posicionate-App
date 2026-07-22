69d2535c20d0fcac70b2da6a7d543170bed68817
Javier Minto Arze <diegominto2@gmail.com>
2026-07-22 12:49:12 -0400

    FIX 6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,24 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(
-            at: '*',
-            headers: Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO |
-            Request::HEADER_X_FORWARDED_AWS_ELB
-        );
-
         $middleware->alias([
             'vigente' => \App\Http\Middleware\CheckVigencia::class,
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (AuthenticationException $e, Request $request) {
-            if ($request->expectsJson() || $request->is('whatsapp/*')) {
-                return response()->json(['message' => 'Unauthenticated.'], 401);
-            }
-        });
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
     })->create();
